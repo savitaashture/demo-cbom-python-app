2c7b012c5d5e0e710c7a0a7b62eb9d72c439a0fd
Anitha Natarajan <anataraj@redhat.com>
2025-06-01 15:39:45 +0530

    Enhanced the logoc to be processabel by CBOMKit

--- app/crypto.py
+++ app/crypto.py
@@ -6,7 +6,7 @@
 | -------------- | ----------------------------- |
 | Hardcoded Key  | Easy to guess/crack           |
 | Fixed IV       | Vulnerable to pattern attacks |
-| CBC Mode       | No integrity check            |
+| ECB Mode       | No integrity check            |
 | Manual Padding | Easy to corrupt or exploit    |
 | No KDF or Salt | Password reused as-is         |
 '''
@@ -17,16 +17,16 @@ from cryptography.hazmat.backends import default_backend
 import base64
 
 def encrypt_user_data(username, password):
-    key = b'0123456789abcdef'  # Hardcoded static key (BAD)
-    iv = b'\x00' * 16          # Fixed IV (BAD)
-    
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))  # No integrity mode
+    key = b'12345678'  # Hardcoded weak key
+    iv = b'\x00' * 8  # Fixed IV
+
+    cipher = Cipher(algorithms.DES(key), modes.ECB(), backend=default_backend())  # Insecure ECB mode
     encryptor = cipher.encryptor()
 
     plaintext = f"{username}:{password}".encode()
 
-    # Manual, improper padding (and insecure mode)
-    padding_length = 16 - (len(plaintext) % 16)
+    # Manual padding
+    padding_length = 8 - (len(plaintext) % 8)
     padded = plaintext + bytes([padding_length]) * padding_length
 
     ciphertext = encryptor.update(padded) + encryptor.finalize()
